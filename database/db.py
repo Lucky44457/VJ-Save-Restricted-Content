0912f9b16e31a6b4f18312324fd19177122f801a
Lucky44457 <maribelwate@gmail.com>
2025-08-02 14:24:03 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -10,10 +10,9 @@ class Database:
 
     def new_user(self, id, name):
         return dict(
-            id=id,
-            name=name,
-            session=None,
-            replace=None
+            id = id,
+            name = name,
+            session = None,
         )
     
     async def add_user(self, id, name):
@@ -21,7 +20,7 @@ class Database:
         await self.col.insert_one(user)
     
     async def is_user_exist(self, id):
-        user = await self.col.find_one({'id': int(id)})
+        user = await self.col.find_one({'id':int(id)})
         return bool(user)
     
     async def total_users_count(self):
@@ -39,22 +38,6 @@ class Database:
 
     async def get_session(self, id):
         user = await self.col.find_one({'id': int(id)})
-        return user.get('session') if user else None
-
-    async def set_replace(self, id, old, new):
-        await self.col.update_one(
-            {'id': int(id)},
-            {'$set': {'replace': {'old': old, 'new': new}}}
-        )
-
-    async def get_replace(self, id):
-        user = await self.col.find_one({'id': int(id)})
-        return user.get('replace') if user else None
-
-    async def clear_replace(self, id):
-        await self.col.update_one(
-            {'id': int(id)},
-            {'$set': {'replace': None}}
-        )
+        return user.get('session')
 
 db = Database(DB_URI, DB_NAME)
